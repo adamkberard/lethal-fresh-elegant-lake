461c7b2a270ef997155fbdb6b631180dbf7fe740
Adam <adamkberard@gmail.com>
2021-08-13 17:26:01 -0700

    added flake8 file

--- lethalFreshElegantLake/settings.py
+++ lethalFreshElegantLake/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
