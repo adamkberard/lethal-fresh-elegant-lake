fff568e798e85b527a3dd7b6e59f07102a90315c
Adam <adamkberard@gmail.com>
2021-08-17 22:30:16 -0500

    added second getting test

--- pizza_order/tests/test_pizza_gets.py
+++ pizza_order/tests/test_pizza_gets.py
@@ -35,3 +35,34 @@ class Test_Pizza_Getting_Single_Pizzas(MyTestCase):
         self.assertEqual(responseData[0]['crust'], PizzaOrder.CRUST_THIN)
         self.assertEqual(responseData[0]['Order_ID'], pizza.order_id)
         self.assertEqual(responseData[0]['Table_No'], 30000)
+
+    def test_get_single_pizza_all_options(self):
+        authUser = CustomUserFactory()
+        
+        for flavor in [x[0] for x in PizzaOrder.FLAVOR_CHOICES]:
+            for size in [x[0] for x in PizzaOrder.SIZE_CHOICES]:
+                for crust in [x[0] for x in PizzaOrder.CRUST_CHOICES]:
+                    pizza = PizzaOrder.objects.create(
+                        flavor=flavor,
+                        crust=crust,
+                        size=size,
+                        table_number=30000,
+                        ordered_by=authUser
+                    )
+                    client = APIClient()
+                    client.force_authenticate(user=authUser)
+                    response = client.get(reverse('pizza_create_list'))
+
+                    self.assertResponse200(response)
+                    responseData = self.loadJSONSafely(response)
+
+                    # Now we check the data, make sure it is accurate
+                    self.assertEqual(len(responseData), 1)
+                    self.assertEqual(responseData[0]['flavor'], flavor)
+                    self.assertEqual(responseData[0]['size'], size)
+                    self.assertEqual(responseData[0]['crust'], crust)
+                    self.assertEqual(responseData[0]['Order_ID'], pizza.order_id)
+                    self.assertEqual(responseData[0]['Table_No'], 30000)
+
+                    # Now delete the queryset so it starts fresh every loop
+                    PizzaOrder.objects.all().delete()
